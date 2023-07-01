3a7b3bbb52a665463e41d0503605f5612a548abf
Nick van Loo <58033512+NickvanLoo@users.noreply.github.com>
2023-07-01 16:36:58 +0200

    update

--- portfolio-website2023/src/pages/Projects.js
+++ portfolio-website2023/src/pages/Projects.js
@@ -1,11 +1,21 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import "../styles/Project.css";
+import MijnGithub from "../assets/GitHub.png";
+//import GithubIcon from "@material-ui/icons/GitHub";
 
 function Projects() {
     return (
         <div className="projects">
-            <h3>Deze pagina is nog in aanbouw en komt binnenkort online!</h3>
+            <h1>Mijn Projecten</h1>
+            <h3>Deze pagina is nog in ontwikkeling!</h3>
+            <div className="projectList">
+                <a href="https://github.com/NickvanLoo" target="_blank">
+                <ProjectItem name="Mijn github" image={MijnGithub} />
+                </a>
+            </div>
         </div>
     )
 }
 
-export default Projects
+export default Projects;
